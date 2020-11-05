46c917c3269fdffc4f7b8dc8bcd27b287a5b16b6
SarahHayat <saraahyt@gmail.com>
2020-11-05 14:02:17 +0100

    get/post/delete/patch

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const defaultRouter = require('./routes/routes')
+const defaultRouter = require('./routes')
 const app = express();
 
-app.listen(3000);
+app.listen(3000, (res, req) =>{
+    console.log('listening on http://localhost:3000')
+});
 
 app.use(express.json())
-app.use(express.urlencoded({extend:true}))
+app.use(express.urlencoded({ extended: true }))
 app.use('/api', defaultRouter)
 
 app.get('/',(request, response)=>{
--- routes.js
+++ routes.js
@@ -1,28 +1,53 @@
 const Router = require('express').Router
 const router = Router();
 const store = {
-    ressources: {
-        res_id: {name: 'xx'}
+    resources: {
+        res_id: {
+            id : 'ceci est un id',
+            name: 'xx',
+            
+        }
     }
 }
 
-router.get('/ressources/:id', (req, res) => {
+router.get('/resources/:id', (req, res) => {
     console.log(req.params)
     const idx = "l'id est " + req.params.id
     res.send(idx)
 })
 
-router.post('/ressources/', (req, res) => {
-    const stor1 = {
-        ressources: {
-            res_id: {name: 'Sarah'},
-        },
+router.post('/resources/', (req, res) => {
+    const resource = req.body
+    resource.id = Object.keys(store.resources.lenght + 1)
+    store.resources[resource.id] = resource
+    res.json()
+})
+
+router.put('/resources/:id', (req,res)=>{
+    const id = req.params
+    if( req.params.id === req.body.id ){
+        store.ressurces[id] = req.body
+    }
+    else{
+        res.status(400).end()
     }
 })
 
-router.patch('/ressources/:id', (req,res)=>{
-
+router.delete('/resources/:id', (req, res) =>{
+    const { id } = req.params
+    if (store.resources[id]){
+        delete store.resources[id]
+         res.json({success : true})
+    }
+    else {
+        res.status(400).end()
+    }
 })
 
-
+router.patch('/resources/:id', (req, res)=>{
+    const id = req.params.id
+    const resource = {...store.resources[id], ...req.body}
+    store.ressurces[id] = resource
+    res.json()
+})
 module.exports = router
